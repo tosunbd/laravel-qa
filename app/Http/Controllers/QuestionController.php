5e88beaacbd4e907a2809cb1a24488835a45ef7c
shadheen <tosunbd@gmail.com>
2019-03-31 01:01:12 +0600

    Extend the form fuctionality to be able to update question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -50,7 +50,7 @@ class QuestionController extends Controller
         //dd('store');
 
         //return redirect('/questions');
-        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted.");
     }
 
     /**
@@ -72,7 +72,7 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view("questions.edit", compact('question'));
     }
 
     /**
@@ -82,9 +82,11 @@ class QuestionController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title','body'));
+
+        return redirect('/questions')->with('success', "Your question has been updated.");
     }
 
     /**
